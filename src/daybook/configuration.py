5faeef54853149744b8e6ad179b1f368414402b0
Simon Wells <siwells@gmail.com>
2013-06-06 15:08:08 +0100

    Set max size for each individual log file to 10MB

--- src/daybook/configuration.py
+++ src/daybook/configuration.py
@@ -24,7 +24,7 @@ def init(app):
 
 def logs(app):
     log_pathname = app.config['log_location'] + app.config['log_file']
-    file_handler = RotatingFileHandler(log_pathname, maxBytes=1024* 1024 * 100 , backupCount=1024)
+    file_handler = RotatingFileHandler(log_pathname, maxBytes=1024* 1024 * 10 , backupCount=1024)
     file_handler.setLevel( app.config['log_level'] )
     formatter = logging.Formatter("%(levelname)s | %(asctime)s |  %(module)s | %(funcName)s | %(message)s")
     file_handler.setFormatter(formatter)
