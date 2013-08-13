16a507cbe5fe8b2c4caa46a5a1b14a91689d3f4a
Simon Wells <siwells@gmail.com>
2013-08-13 16:18:57 +0100

    Add new entries to data DB

--- src/daybook/data.py
+++ src/daybook/data.py
@@ -2,5 +2,5 @@ import couchdb
 import json
 
 def add_entry(db, uuid, entry):
-    print str(db), uuid, json.dumps(entry)
-    print "Adding entry"
+    doc = {"uuid": uuid, "data": entry}
+    doc_id = db.save(doc)
