10f2fdad25aecd4a5a8369a33f12f8e4b71f2103
Simon Wells <siwells@gmail.com>
2013-08-13 12:01:04 +0100

    Do configuration for user DB

--- src/daybook/configuration.py
+++ src/daybook/configuration.py
@@ -16,6 +16,11 @@ def init(app):
         app.config['SECRET_KEY'] = config.get("daybook", "secret_key")
         app.config['ip_address'] = config.get("daybook", "ip_address")
         app.config['port'] = config.get("daybook", "port")
+
+        app.config['db_name'] = config.get("db", "name")
+        app.config['db_ipaddress'] = config.get("db", "ip_address")
+        app.config['db_port'] = config.get("db", "port")
+
         app.config['log_file'] = config.get("logging", "name")
         app.config['log_location'] = config.get("logging", "location")
         app.config['log_level'] = config.get("logging", "level")
