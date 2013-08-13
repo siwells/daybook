11a42ffa5ad0715b1245d4d2125fc6c83d22061d
Simon Wells <siwells@gmail.com>
2013-08-13 22:06:43 +0100

    Support for sending emails from the app

---
+++ src/daybook/mail.py
@@ -0,0 +1,39 @@
+import smtplib
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+
+import os
+
+def send(gmail_user, gmail_pwd, to, subject, text, attach=None):
+    msg = MIMEMultipart()
+    
+    msg['From'] = gmail_user
+    msg['To'] = to
+    msg['Subject'] = subject
+    
+    msg.attach(MIMEText(text))
+
+    if attach is not None:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(open(attach, 'rb').read())
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attach))
+        msg.attach(part)
+    
+    print msg
+
+    mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmail_user, gmail_pwd)
+    mailServer.sendmail(gmail_user, to, msg.as_string())
+    # Should be mailServer.quit(), but that crashes...
+    mailServer.close()
+
+if __name__ == '__main__':
+
+    send("siwells@gmail.com", "Hello from python!", "This is a email sent with python")
