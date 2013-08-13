3c7366fe177aafa22c2b7901ea021a8c76704832
Simon Wells <siwells@gmail.com>
2013-08-13 18:23:07 +0100

    Function to retrieve number of entries for a specified user

--- src/daybook/data.py
+++ src/daybook/data.py
@@ -6,17 +6,21 @@ def add_entry(db, uuid, entry):
     doc_id = db.save(doc)
 
 def get_entries(db, uuid):
-    print "Entries for user: ", uuid
     entries = []
     for row in db.view('entries/get_entries'):
         if row.key == uuid:
-            entries.append(row.value)
-            #print row.key, json.dumps(row.value['data'])
+            entries.append(row.value['data'])
     return entries
 
+def get_entry_count(db, uuid):
+    count = 0
+    for row in db.view('entries/get_entries'):
+        if row.key == uuid:
+            count = count + 1
+
+    return count
 
 def get_entry_list(db, uuid):
-    print "Entry list for user: ", uuid
     entrylist = []
     idx = 1
     for row in db.view('entries/get_entries'):
@@ -25,4 +29,5 @@ def get_entry_list(db, uuid):
             entrylist.append( {"id": idx, "date": row.value['data']['date']})
             idx = idx + 1
     return entrylist
+
 
